ebd2a95adc8c786736a7655bc6d20497afb49de9
ArtemSorin <95878097+ArtemSorin@users.noreply.github.com>
2022-04-03 14:36:38 +0300

    update_03_04_2022

--- Blockchain Basics/Blockchain Basics/FirstGamePage.xaml.cs
+++ Blockchain Basics/Blockchain Basics/FirstGamePage.xaml.cs
@@ -23,15 +23,16 @@ namespace Blockchain_Basics
             {
                 byte[] bytes = Encoding.Unicode.GetBytes(s);
 
-                MD5CryptoServiceProvider CSP =
-                    new MD5CryptoServiceProvider();
+                MD5CryptoServiceProvider CSP = new MD5CryptoServiceProvider();
 
                 byte[] byteHash = CSP.ComputeHash(bytes);
 
                 string hash = string.Empty;
 
                 foreach (byte b in byteHash)
+                {
                     hash += string.Format("{0:x2}", b);
+                }
 
                 return hash;
             }
